f0b92f8c32c0e45bd023e8e112c9280c013cb00f
erkela <odin.bartusiak@gmail.com>
2022-11-02 16:08:40 +0100

    Delete serviceworker.js

--- serviceworker.js
+++
@@ -1,8 +0,0 @@
-importScripts("./sw_modules/cache.js");
-self.addEventListener("message",function(e){
-  console.log(e)
-})
-
-self.addEventListener("periodicsync",function(event){
-  console.log(event)
-})
